d5fe546816cc3f85eb68e4fe78a7801aee075562
devinswenson <devin.swenson@affirm.com>
2024-02-14 13:55:05 -0600

    Rules are all now working

--- main.py
+++ main.py
@@ -2,24 +2,9 @@
 from __future__ import print_function
 import numpy as np
 
-# Add your datapoints in the array below, ordered by most recent datapoints to oldest datapoints
-datapoints = []
-
-mean = 0
-stdev = 0
-
-
-def generate_reference_values(dataset):
-    mean = round(sum(dataset) / len(dataset))
-    stdev = round(np.std(dataset))
-
-    print("--generate_reference_values--")
-    print("mean: " + str(mean))
-    print("stdev: " + str(stdev))
-
 
 # Check if any data point is < -3 STDEV
-def rule_1():
+def rule_1(datapoints, mean, stdev):
     i = 0
 
     while i < len(datapoints):
@@ -27,11 +12,11 @@ def rule_1():
             print("Rule 1 triggered - " + str(datapoints[i]) + " falls below -3 standard deviations from the mean")
             return True
         i += 1
-    print("Rule 1 not triggered")
+    # print("Rule 1 not triggered")
 
 
 # Nine or more data points in a row below the mean
-def rule_2():
+def rule_2(datapoints, mean, stdev):
     i = 1
     count = 0
 
@@ -44,11 +29,11 @@ def rule_2():
             print("Rule 2 triggered - 9 or more points in a row below the mean")
             return True
         i += 1
-    print("Rule 2 not triggered")
+    # print("Rule 2 not triggered")
 
 
 # 6 points in a row are decreasing
-def rule_3():
+def rule_3(datapoints, mean, stdev):
     i = 1
     count = 0
 
@@ -61,11 +46,11 @@ def rule_3():
             print("Rule 3 triggered - six points in a row are decreasing")
             return True
         i += 1
-    print("Rule 3 not triggered")
+    # print("Rule 3 not triggered")
 
 
 # 14 points in a row are alternating, increase then decrease
-def rule_4():
+def rule_4(datapoints, mean, stdev):
     i = 1
     count = 0
 
@@ -79,11 +64,11 @@ def rule_4():
             print("Rule 4 triggered - 14 consecutive points are alternating, increasing then decreasing")
             return True
         i += 1
-    print("Rule 4 not triggered")
+    # print("Rule 4 not triggered")
 
 
 # 2/3 points in a row are < -2 stdev from mean
-def rule_5():
+def rule_5(datapoints, mean, stdev):
     i = 2
     count = 0
 
@@ -100,11 +85,11 @@ def rule_5():
         else:
             count = 0
         i += 1
-    print("Rule 5 not triggered")
+    # print("Rule 5 not triggered")
 
 
 # 4/5 points in a row are < -1 stdev from mean
-def rule_6():
+def rule_6(datapoints, mean, stdev):
     i = 4
     count = 0
 
@@ -120,11 +105,11 @@ def rule_6():
         else:
             count = 0
         i += 1
-    print("Rule 6 not triggered")
+    # print("Rule 6 not triggered")
 
 
 # 15 points in a row are within 1 stdev from mean, + or -
-def rule_7():
+def rule_7(datapoints, mean, stdev):
     i = 0
     count = 0
 
@@ -137,15 +122,15 @@ def rule_7():
             print("Rule 7 triggered - 15 points in a row are within +/- 1 standard deviation from the mean")
             return True
         i += 1
-    print("Rule 7 not triggered")
+    # print("Rule 7 not triggered")
 
 # 8 points in a row are above or below 1 standard deviation, alternating up and down
-def rule_8():
+def rule_8(datapoints, mean, stdev):
     i = 1
     count = 0
 
     while i < len(datapoints) - 1:
-        if (datapoints[i - 1] < datapoints[i] > datapoints[i + 1]) or (datapoints[i - 1] > datapoints[i] < datapoints[i + 1]) and ((stdev + mean) < datapoints[i] or (-1 * stdev + mean) > datapoints[i]):
+        if (datapoints[i - 1] < datapoints[i] > datapoints[i + 1]) or (datapoints[i - 1] > datapoints[i] < datapoints[i + 1]) and ((stdev + mean) < datapoints[i] or (mean - stdev) > datapoints[i]):
             count += 1
         else:
             count = 0
@@ -153,10 +138,13 @@ def rule_8():
             print("Rule 8 triggered - 8 points in a row are above or below 1 standard deviation, alternating up and down")
             return True
         i += 1
-    print("Rule 8 not triggered")
+    #print("Rule 8 not triggered")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # Add your datapoints in the array below, ordered by most recent datapoints to oldest datapoints
+    datapoints = [145, 118, 118, 113, 136, 125, 136, 141, 155, 134, 137, 175, 182, 149, 136, 197, 155, 150, 157, 139, 139, 144, 147, 157, 200, 169, 155, 130, 172, 152]
+
     mean = round(sum(datapoints) / len(datapoints))
     stdev = round(np.std(datapoints))
     rules_triggered = []
@@ -164,14 +152,15 @@ if __name__ == '__main__':
     print("--generate_reference_values--")
     print("mean: " + str(mean))
     print("stdev: " + str(stdev))
+    print(datapoints)
 
-    if rule_1(): rules_triggered.append("1")
-    if rule_2(): rules_triggered.append("2")
-    if rule_3(): rules_triggered.append("3")
-    if rule_4(): rules_triggered.append("4")
-    if rule_5(): rules_triggered.append("5")
-    if rule_6(): rules_triggered.append("6")
-    if rule_7(): rules_triggered.append("7")
-    if rule_8(): rules_triggered.append("8")
+    if rule_1(datapoints, mean, stdev): rules_triggered.append("1")
+    if rule_2(datapoints, mean, stdev): rules_triggered.append("2")
+    if rule_3(datapoints, mean, stdev): rules_triggered.append("3")
+    if rule_4(datapoints, mean, stdev): rules_triggered.append("4")
+    if rule_5(datapoints, mean, stdev): rules_triggered.append("5")
+    if rule_6(datapoints, mean, stdev): rules_triggered.append("6")
+    if rule_7(datapoints, mean, stdev): rules_triggered.append("7")
+    if rule_8(datapoints, mean, stdev): rules_triggered.append("8")
 
     print(str(rules_triggered)[1:-1])
